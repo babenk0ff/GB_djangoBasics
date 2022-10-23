6c4cfa12fa7e698429bf2c4793e5d4753ef45517
babenk0ff <babenk0fff@gmail.com>
2022-10-23 23:01:18 +0300

    lesson-3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,7 @@
+import json
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.http import HttpResponseRedirect
 
 
 class ContactsView(TemplateView):
@@ -23,3 +26,20 @@ class LoginView(TemplateView):
 
 class NewsView(TemplateView):
     template_name = 'mainapp/news.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+
+        context_data['news_title'] = 'Новостной заголовок'
+        context_data['news_preview'] = 'Превью новости'
+        context_data['range'] = range(5)
+
+        with open(settings.BASE_DIR / 'mainapp/static/mainapp/news.json') as news_file:
+            context_data['object_list'] = json.load(news_file)
+
+        return context_data
+
+
+def google_redirect_view(request):
+    search_string = request.GET.get('search_string')
+    return HttpResponseRedirect(f'https://www.google.com/search?q={search_string}')
